ff6cd8eb060f493d68ff0ed0d02644e14862895d
aaradhyamohit26 <aaradhya.mohit26@gmail.com>
2026-06-19 21:54:40 +0530

    Update Day5.cpp

--- Week3/Day5.cpp
+++ Week3/Day5.cpp
@@ -1,1 +1,35 @@
+class MyQueue {
+public:
+    stack<int> s1;
+    stack<int> s2;
+
+    MyQueue() {
+        
+    }
+
+    void push(int x) {
+        s1.push(x);
+    }
+
+    int pop() {
+        peek();
+        int ans = s2.top();
+        s2.pop();
+        return ans;
+    }
+
+    int peek() {
+        if(s2.empty()) {
+            while(!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        return s2.top();
+    }
+
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
 
